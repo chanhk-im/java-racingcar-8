cefd156857cb98b31b32722dee5c17a4984aca18
chanhk-im <cfasd1875@gmail.com>
2025-10-25 02:29:25 +0900

    feat: WinnerSelector selectWinner 구현
    - ExceptionMessage 예외 메시지 추가

--- src/main/java/racingcar/message/ExceptionMessage.java
+++ src/main/java/racingcar/message/ExceptionMessage.java
@@ -3,5 +3,9 @@ package racingcar.message;
 public class ExceptionMessage {
     private ExceptionMessage() {}
 
+    /* ------------- Car ------------- */
     public static String CAR_MOVE_OVERFLOW_MESSAGE = "위치 값이 너무 큽니다.";
+
+    /* ------------- WinnerSelector ------------- */
+    public static String WINNER_SELECTOR_NO_CARS = "등록된 자동차가 없습니다.";
 }
--- src/main/java/racingcar/service/WinnerSelector.java
+++ src/main/java/racingcar/service/WinnerSelector.java
@@ -1,4 +1,31 @@
 package racingcar.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.message.ExceptionMessage;
+
 public class WinnerSelector {
+    private WinnerSelector() {}
+
+    public static List<Car> selectWinner(List<Car> cars) {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNER_SELECTOR_NO_CARS);
+        }
+
+        int maxPosition = 0;
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+                winners.clear();
+                winners.add(car);
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
+    }
 }
