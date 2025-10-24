fba9118f22c1f5729f47001a5c4396a7bbc01784
chanhk-im <cfasd1875@gmail.com>
2025-10-25 03:04:55 +0900

    fix: 상수 선언 시 final 추가

---
+++ src/main/java/racingcar/consts/RacingConstants.java
@@ -0,0 +1,4 @@
+package racingcar.consts;
+
+public class RacingConstants {
+}
--- src/main/java/racingcar/message/ExceptionMessage.java
+++ src/main/java/racingcar/message/ExceptionMessage.java
@@ -4,8 +4,8 @@ public class ExceptionMessage {
     private ExceptionMessage() {}
 
     /* ------------- Car ------------- */
-    public static String CAR_MOVE_OVERFLOW_MESSAGE = "위치 값이 너무 큽니다.";
+    public final static String CAR_MOVE_OVERFLOW_MESSAGE = "위치 값이 너무 큽니다.";
 
     /* ------------- WinnerSelector ------------- */
-    public static String WINNER_SELECTOR_NO_CARS = "등록된 자동차가 없습니다.";
+    public final static String WINNER_SELECTOR_NO_CARS = "등록된 자동차가 없습니다.";
 }
