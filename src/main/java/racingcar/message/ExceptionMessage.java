48190e7a75d16505b189e8a4551439581c4471e1
chanhk-im <cfasd1875@gmail.com>
2025-10-26 02:20:32 +0900

    feat: InputManager 구현

--- src/main/java/racingcar/message/ExceptionMessage.java
+++ src/main/java/racingcar/message/ExceptionMessage.java
@@ -12,4 +12,11 @@ public class ExceptionMessage {
 
     /* ------------- RacingService ------------- */
     public final static String RACING_SERVICE_NO_CARS = "등록된 자동차가 없습니다.";
+
+    public final static String INPUT_TOO_MANY_NAMES = "입력받은 이름이 너무 많습니다.";
+    public final static String INPUT_TOO_LONG_NAME = "이름이 너무 깁니다.";
+    public final static String INPUT_WRONG_CHARACTER = "이름은 영문 또는 숫자로만 이루어져야 합니다.";
+    public final static String INPUT_NEGATIVE = "양수를 입력해야 합니다.";
+    public final static String INPUT_NOT_A_NUMBER = "숫자가 아닙니다.";
+    public final static String INPUT_TOO_LARGE_NUMBER = "너무 큰 숫자입니다.";
 }
--- src/main/java/racingcar/ui/InputManager.java
+++ src/main/java/racingcar/ui/InputManager.java
@@ -1,6 +1,86 @@
 package racingcar.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.consts.RacingConstants;
+import racingcar.message.ExceptionMessage;
+
 public class InputManager {
     private InputManager() {
+    }
+
+    public static List<String> getInputCarNames() {
+        String input = Console.readLine();
+        Console.close();
+
+        return parseInputCarNames(input);
+    }
+
+    private static List<String> parseInputCarNames(String input) {
+        String[] names = input.split(",");
+        checkTooManyNames(names);
+
+        List<String> carNames = new ArrayList<>();
+
+        for (String name : names) {
+            String cleanName = name.trim();
+
+            checkTooLongName(cleanName);
+            checkWrongCharInName(cleanName);
+
+            carNames.add(cleanName);
+        }
+
+        return carNames;
+    }
+
+    private static void checkTooManyNames(String[] names) {
+        if (names.length > RacingConstants.MAX_CARS_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_TOO_MANY_NAMES);
+        }
+    }
+
+    private static void checkTooLongName(String name) {
+        if (name.length() > RacingConstants.MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_TOO_LONG_NAME);
+        }
+    }
+
+    private static void checkWrongCharInName(String name) {
+        if (!name.matches("^[a-zA-Z0-9]*$")) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_WRONG_CHARACTER);
+        }
+    }
+
+    public static int getInputRaceCount() {
+        String input = Console.readLine();
+
+        checkRaceCountNaN(input);
+
+        int raceCount = Integer.parseInt(input);
+
+        checkRaceCountNegative(raceCount);
+        checkRaceCountTooLarge(raceCount);
+
+        return raceCount;
+    }
+
+    private static void checkRaceCountNaN(String input) {
+        if (!input.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_NOT_A_NUMBER);
+        }
+    }
+
+    private static void checkRaceCountNegative(int raceCount) {
+        if (raceCount < 0) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_NEGATIVE);
+        }
+    }
+
+    private static void checkRaceCountTooLarge(int raceCount) {
+        if (raceCount > RacingConstants.MAX_RACE_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_TOO_LARGE_NUMBER);
+        }
     }
 }
