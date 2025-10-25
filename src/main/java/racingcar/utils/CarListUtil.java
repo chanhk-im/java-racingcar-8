be8a14bd49ca3724e09cdf87c65af0166fa33980
chanhk-im <cfasd1875@gmail.com>
2025-10-25 16:32:45 +0900

    feat: CarListUtil carListToString 구현

--- src/main/java/racingcar/utils/CarListUtil.java
+++ src/main/java/racingcar/utils/CarListUtil.java
@@ -1,4 +1,20 @@
 package racingcar.utils;
 
+import java.util.List;
+import java.util.StringJoiner;
+import racingcar.domain.Car;
+
 public class CarListUtil {
+    private CarListUtil() {
+    }
+
+    public static String carListToString(List<Car> cars) {
+        StringJoiner joiner = new StringJoiner(", ");
+
+        for (Car car : cars) {
+            joiner.add(car.getName());
+        }
+
+        return joiner.toString();
+    }
 }
