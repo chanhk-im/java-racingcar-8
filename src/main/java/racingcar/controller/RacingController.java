aef63deddf4f23567a1b27a471adac2d0c7bdc06
chanhk-im <cfasd1875@gmail.com>
2025-10-26 02:59:00 +0900

    feat: RacingController 우승자 출력 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.dto.RacingInformation;
+import racingcar.dto.RacingWinners;
 import racingcar.service.RacingService;
 import racingcar.ui.InputManager;
 import racingcar.ui.UserView;
@@ -25,6 +26,8 @@ public class RacingController {
         List<Car> cars = CarUtil.carListFromStringArray(carNames);
 
         RacingInformation racingInformation = RacingInformation.of(cars, raceCount);
-        racingService.race(racingInformation);
+        RacingWinners racingWinners = racingService.race(racingInformation);
+
+        UserView.showWinners(racingWinners.getWinners());
     }
 }
