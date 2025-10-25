2e3ba258b4027df7bb0ed595b1e38b99b6cd5e7a
chanhk-im <cfasd1875@gmail.com>
2025-10-25 17:00:55 +0900

    test: CarUtilTest carInfoString test 작성

--- src/test/java/racingcar/utils/CarUtilTest.java
+++ src/test/java/racingcar/utils/CarUtilTest.java
@@ -6,7 +6,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
-public class CarListUtilTest {
+public class CarUtilTest {
+    @Test
+    @DisplayName("carInfoString은 car name, position을 string으로 반환해야 한다.")
+    void carInfoString() {
+        Car car = Car.of("test", 5);
+
+        String expected = "test : -----";
+        String actual = CarUtil.carInfoString(car);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     @DisplayName("carListToString은 car name을 \", \"으로 구분된 string을 반환해야 한다.")
     void carListToString() {
@@ -17,7 +28,7 @@ public class CarListUtilTest {
         List<Car> cars = List.of(carA, carB, carC);
 
         String expected = "A, B, C";
-        String actual = CarListUtil.carListToString(cars);
+        String actual = CarUtil.carListToString(cars);
 
         Assertions.assertEquals(expected, actual);
     }
@@ -28,7 +39,7 @@ public class CarListUtilTest {
         List<Car> cars = List.of();
 
         String expected = "";
-        String actual = CarListUtil.carListToString(cars);
+        String actual = CarUtil.carListToString(cars);
 
         Assertions.assertEquals(expected, actual);
     }
