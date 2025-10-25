010d15f8ecd5bdb4565fea9645e8457f6e5cf446
chanhk-im <cfasd1875@gmail.com>
2025-10-25 17:08:27 +0900

    feat: UserView 구현

--- src/main/java/racingcar/consts/RacingConstants.java
+++ src/main/java/racingcar/consts/RacingConstants.java
@@ -10,7 +10,8 @@ public class RacingConstants {
     public final static int RACE_RANDOM_MIN_NUMBER = 0;
     public final static int RACE_RANDOM_MAX_NUMBER = 9;
 
-    public final static String USER_VIEW_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String USER_VIEW_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String USER_VIEW_INPUT_RACE_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public final static String USER_VIEW_RESULT_HEADER_MESSAGE = "실행 결과";
     public final static String USER_VIEW_WINNERS_MESSAGE = "최종 우승자 : ";
--- src/main/java/racingcar/ui/UserView.java
+++ src/main/java/racingcar/ui/UserView.java
@@ -1,4 +1,35 @@
 package racingcar.ui;
 
+import java.util.List;
+import racingcar.consts.RacingConstants;
+import racingcar.domain.Car;
+import racingcar.utils.CarUtil;
+
 public class UserView {
+    private UserView() {
+    }
+
+    public static void showInputCarNameMessage() {
+        System.out.println(RacingConstants.USER_VIEW_INPUT_CAR_NAME_MESSAGE);
+    }
+
+    public static void showInputRaceCountMessage() {
+        System.out.println(RacingConstants.USER_VIEW_INPUT_RACE_COUNT_MESSAGE);
+    }
+
+    public static void showWinners(List<Car> winners) {
+        String winnerString = CarUtil.carListToString(winners);
+        System.out.print(RacingConstants.USER_VIEW_WINNERS_MESSAGE);
+        System.out.println(winnerString);
+    }
+
+    public static void showRaceResultMessage() {
+        System.out.println(RacingConstants.USER_VIEW_RESULT_HEADER_MESSAGE);
+    }
+
+    public static void showRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(CarUtil.carInfoString(car));
+        }
+    }
 }
